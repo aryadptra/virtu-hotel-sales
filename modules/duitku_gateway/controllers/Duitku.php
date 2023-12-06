8338ceec17d5209d2aa756ac24caaa273ad0bb1e
Arya Dwi Putra <aryadptr.business@gmail.com>
2023-12-06 12:42:47 +0700

    Co-authored-by: Arya Dwi Putra <aryaadwptr@gmail.com>

---
+++ modules/duitku_gateway/controllers/Duitku.php
@@ -0,0 +1,17 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Duitku extends AdminController
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        // $this->duitku_gateway->getPaymentMethods();
+        // $this->duitku_gateway->requestTransaction();
+    }
+}
